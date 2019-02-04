fa87e4b87d3a775469a89afd050f677a84cf36e7
oscarvial55@gmail.com <hacker6@OTAProd74ops.onmicrosoft.com>
2019-02-04 23:58:25 +0000

    loop tests

--- __tests__/Features/sample.feature
+++ __tests__/Features/sample.feature
@@ -3,8 +3,20 @@ Feature: Feature fuckit
    Feature Description
 
 
-Scenario: It Does a thing
+Scenario: Color change
 
-Given The fucker loads
-And i can click it 
-Then it should do shit
+Given The user can load the page
+And can click onto the button
+Then it should change the colour
+
+Scenario Outline: Drone Multiple Journies
+Given The drone has <Fuel> 
+And the first dropoff is at <locationA>
+And the second location is at <locationB>
+Then i would expect the drone to <response> the Order
+
+Examples:
+| Fuel | locationA | locationB          | response |
+| 40   | 30303e 32423w  | 3234e 53454w  | accept   |
+| 40   | 39492394423w  | 3234e 53454w  | decline   |
+| 40   | 30303e 32423w  | 1234e 53454w  | accept   |
--- __tests__/sample.test.ts
+++ __tests__/sample.test.ts
@@ -3,16 +3,34 @@ import { loadFeature, defineFeature } from "jest-cucumber";
 const feature = loadFeature("./__tests__/Features/sample.feature");
 
 defineFeature(feature, (test) => {
-    test('It Does a thing', ({ given, when, then, pending }) => {
-        given('The fucker loads', () => {
+    test('Color change', ({ given, when, then, pending }) => { 
+        given('The user can load the page', () => {            
+            pending();                                         
+        });                                                    
+                                                               
+        given('can click onto the button', () => {             
+            pending();                                         
+        });                                                    
+                                                               
+        then('it should change the colour', () => {            
+            pending();                                         
+        });                                                    
+    });  
+    
+    test('Drone Multiple Journies', ({ given, when, then, pending }) => {
+        given(/^The drone has (.*)$/, (arg0) => {
             pending();
         });
 
-        given('i can click it', () => {
+        given(/^the first dropoff is at (.*)$/, (arg0) => {
             pending();
         });
 
-        then('it should do shit', () => {
+        given(/^the second location is at (.*)$/, (arg0) => {
+            pending();
+        });
+
+        then(/^i would expect the drone to (.*) the Order$/, (arg0) => {
             pending();
         });
     });
